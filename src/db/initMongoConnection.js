c11cf33b3ebc7964379f1e9469c363e94fef9803
unknown <you@example.com>
2024-12-11 15:18:45 +0200

    update DB conection

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -3,13 +3,11 @@ import mongoose from 'mongoose';
 // password  mongodb:  _fbQW_d7p%40nK3DQ  //xXyOqVU4YII12yNt
 // login: korniyenko
 
-
-
 export const initMpngoDB = async () => {
   try {
-   await mongoose.connect(
-     'mongodb+srv://korniyenko:_fbQW_d7p%40nK3DQ@cluster0.bpx7j.mongodb.net/contacts?retryWrites=true&w=majority&appName=Cluster0',
-   );
+    await mongoose.connect(
+      'mongodb+srv://korniyenko:_fbQW_d7p%40nK3DQ@cluster0.bpx7j.mongodb.net/contacts?retryWrites=true&w=majority&appName=Cluster0',
+    );
 
     console.log('Mongo connection successfully');
   } catch (error) {
