014ac8b1fde014986719887335edb2721d3d9e57
unknown <you@example.com>
2024-12-12 13:30:39 +0200

    fix model

--- src/db/models/ContactModel.js
+++ src/db/models/ContactModel.js
@@ -1,27 +1,33 @@
 import { Schema, model } from 'mongoose';
 
-const contactSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+    },
+    isFavourite: {
+      type: Boolean,
+      default: false,
+    },
+    contactType: {
+      type: String,
+      enum: ['work', 'home', 'personal'],
+      required: true,
+      default: 'personal',
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
+  {
+    timestamps: true,
   },
-  phone: {
-    type: String,
-    required: true,
-  },
-  address: {
-    type: String,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
+);
 
 
 const ContactsCollection = model('Contact', contactSchema);
