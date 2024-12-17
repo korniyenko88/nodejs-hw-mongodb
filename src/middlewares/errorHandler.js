d1ca77e4c3810a6c13f0eb6b1f68b9c5dd69df68
unknown <you@example.com>
2024-12-17 16:20:55 +0200

    update errorHandler

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -2,6 +2,7 @@ export const errorHandler = (error, req, res, next) => {
   const { status = 500, message } = error;
   res.status(status).json({
     status,
-    message,
+    message: 'Something went wrong',
+    data: error.message,
   });
 };
