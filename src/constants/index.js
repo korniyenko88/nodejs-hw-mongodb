a7d8a867416da9347c8a1aa2f0bd50a9ba543217
unknown <you@example.com>
2025-01-20 11:06:44 +0200

    update hw6

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,8 @@
 import path from 'node:path';
 
 export const TEMPLATES_DIR = path.resolve('src', 'templates');
+
+
+export const TEMP_UPLOADS_DIR = path.resolve("temp");
+
+export const UPLOADS_DIR = path.resolve("uploads");
